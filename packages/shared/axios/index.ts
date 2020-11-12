8995f69d49888f2a4115bbba754951577edefcd4
Deivison Cardoso <deivisondc@gmail.com>
2020-11-11 22:16:36 -0300

    add env variables

--- packages/shared/axios/index.ts
+++ packages/shared/axios/index.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: path.join(__dirname, '..', '..', '..', '.env') });
 
 const api = axios.create({
-  baseURL: '',
+  baseURL: process.env.API_BASE_URL,
 });
 
 export default api;
